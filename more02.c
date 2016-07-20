b68dd196dc0bae732860d9467136158ab5389122
gettogetto <643274003@qq.com>
2016-07-20 14:06:34 +0800

    code

--- more02.c
+++ more02.c
@@ -51,7 +51,7 @@ void do_more(FILE* fp)
 int see_more(FILE *cmd)
 {
     int c;
-    printf("%\033[7m more? %\033[m");
+    printf("\033[7m more? \033[m");
     while((c=getc(cmd))!=EOF)
     {
         if(c=='q') return 0;
